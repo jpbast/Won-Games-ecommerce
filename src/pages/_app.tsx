8bca1c03e983073354671a800965eaee01192994
Joao <jp_bast@hotmail.com>
2021-05-05 22:05:34 -0300

    Update boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import { GlobalStyles } from '../styles/globals'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Won Games</title>
+      </Head>
+      <GlobalStyles>
+        <Component {...pageProps} />
+      </GlobalStyles>
+    </>
+  )
 }
 
 export default MyApp
